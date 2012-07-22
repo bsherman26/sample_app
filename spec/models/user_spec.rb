ae72ad15be45528874d313509dbd8728d3a3a007
Brian Sherman <bsherman26@hotmail.com>
2012-07-21 23:59:09 -0500

    Add test to verify that the User admin attribute isn't accessible.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,6 +34,18 @@ describe User do
   it { should be_valid }
   it { should_not be_admin }
 
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        User.new(name: "Name",
+                 email: "name@example.com",
+                 password: "foobar",
+                 password_confirmation: "foobar",
+                 admin: true)
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
+
   describe "with admin attribute set to 'true'" do
     before { @user.toggle!(:admin) }
 
