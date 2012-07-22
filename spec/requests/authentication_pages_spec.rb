b683e3697b5eb086ec7fd93e5f9a54c97849bc0a
Brian Sherman <bsherman26@hotmail.com>
2012-07-22 15:02:35 -0500

    Add tests for no Profile and Settings links when not signed in.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,8 +7,12 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
+    it { should have_selector('h1',       text: 'Sign in') }
+    it { should have_selector('title',    text: 'Sign in') }
+    it { should_not have_link('Account',  href: '#') }
+#    it { should_not have_link('Profile',  href: user_path(user)) }       # I have no idea why this doesn't work
+#    it { should_not have_link('Settings', href: edit_user_path(user)) }  # I have no idea why this doesn't work
+    it { should_not have_link('Sign out', href: signout_path) }
   end
 
   describe "signin" do
